16292a374fd41cf4447778052cf330442e06cf59
Ed <edwoulfe@gmail.com>
2021-06-21 11:59:41 -0400

    Was able to select all the reagents html nodes and filter through them looking for only the ones we need. Next is to grab the properties from the html string.

--- scrape_recipe_data.js
+++ scrape_recipe_data.js
@@ -22,17 +22,47 @@ const options = {
 
 
         // Loop through all the recipe urls, opening and closing a new page for each.
-        for (let i = 0; i < recipeUrls.length; i++) {
+        // for (let i = 0; i < recipeUrls.length; i++) {
 
-            const page = await browser.newPage();
-            await page.setDefaultNavigationTimeout(0);
-            await page.goto(recipeUrls[i]);
+        //     const page = await browser.newPage();
+        //     await page.setDefaultNavigationTimeout(0);
+        //     await page.goto(recipeUrls[i]);
 
 
 
-            await page.close();
+        //     await page.close();
 
-        }
+        // }
+
+        const page = await browser.newPage();
+        await page.setDefaultNavigationTimeout(0);
+        await page.goto('https://tbc.wowhead.com/spell=26754/spellfire-robe');
+
+        // Variable to hold the number of possible reagents we need to loop through.
+        let listLength = await page.evaluate(() => {
+            return document.querySelectorAll('#icon-list-reagents > tbody > tr').length;
+        });
+
+
+        let reagents = await page.evaluate(() => {
+            let htmlNodes = Array.from(document.querySelectorAll('#icon-list-reagents > tbody > tr'));
+
+            let reagentsList = htmlNodes.filter(html => !html.outerHTML.includes('style="display:none"'));
+
+            let reagentsHtml = reagentsList.map((value) => {
+                return value.outerHTML;
+            })
+
+            return reagentsHtml;
+        });
+
+        console.log(reagents);
+        // .outerHTML.includes('style="display:none"')
+
+
+
+
+        await page.close();
 
 
 
